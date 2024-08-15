e1f3f0a308548781d772d66273b7eaaf3e6ed993
Ilka <Ilka@LAPTOP-VAVOB6QC>
2024-08-15 19:17:28 +0200

    Add project files.

---
+++ Module1Lesson11ExerciseDebug/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Module1Lesson11ExerciseDebug
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Debug");
+
+            int a = 25;
+            int b = 5;
+
+            double product = a * b;
+
+            Console.WriteLine(product);
+
+            double quotient = a / b;
+
+            Console.WriteLine(quotient);
+
+            SumSum sumObject = new SumSum();
+            double sumResult = sumObject.CalculateSum();
+
+            Console.WriteLine(sumResult);
+        }
+    }
+}
---
+++ Module1Lesson11ExerciseDebug/SumSum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1Lesson11ExerciseDebug
+{
+    public class SumSum
+    {
+        int c = 4;
+        int d = 5;
+        public double CalculateSum()
+        {
+        return c + d;
+        }
+    }
+}
